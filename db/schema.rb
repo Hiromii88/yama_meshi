d047a7da08da2ca95ccf6f784a439df6449287fa
Hiromii88 <goeson88@gmail.com>
2025-09-27 21:29:40 +0900

    Add LINE link feature (clean version)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_19_071035) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_27_051414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_19_071035) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "line_user_id"
+    t.string "line_link_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
